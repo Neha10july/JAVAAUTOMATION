8424413c0ccdd2bfcb15d92b60abac3f296447ec
Neha Shah <nehabansal5sep@gmail.com>
2025-06-22 11:14:56 +0530

    primitive data

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+class Main {
+    public static void main(String[] args) {
+        String str = "https://www.programiz.com/java-programming/onlin";
+
+        int start = 0;
+        int length = 1;  // initial length of substring
+        int count = 1;
+
+        while (start < str.length()) {
+            int end = start + length;
+            if (end > str.length()) {
+                end = str.length();
+            }
+
+            String extracted = str.substring(start, end);
+            System.out.println(count++ + ": " + extracted);
+
+            // move start to the next position after extracted substring
+            start = end;
+            length++;  // increase length for next substring
+        }
+    }
+}
