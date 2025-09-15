4a57acc5c7b2a6ff8f1e457867a923818fec9ca6
Neha Shah <nehabansal5sep@gmail.com>
2025-09-15 05:50:23 +0530

    15sep all file

---
+++ src/test/java/day38/DisableAutomatedmessageonscreen.java
@@ -0,0 +1,27 @@
+package day38;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.sql.SQLOutput;
+
+public class DisableAutomatedmessageonscreen {
+    public static void main(String[] args) {
+        ChromeOptions options = new ChromeOptions();
+        options.setExperimentalOption("excludeSwitches", new String[] {"enable-automation"});
+
+        WebDriver driver = new ChromeDriver(options);
+
+        driver.get("https://demo.nopcommerce.com/");
+        String act_title = driver.getTitle();
+
+        if (act_title.equals("nopCommerce demo store. Home page title")){
+            System.out.println("Test Passed");
+        }
+        else{
+            System.out.println("Test Failed");
+        }
+
+    }
+}
